d99561388300b133d988d89b8b16081737fc36bb
cesaryraziel <cesario.fcf@hotmail.com>
2023-10-18 17:35:52 -0300

    trabalhando com se else e calculo de medias

---
+++ js/manipular_texto.js
@@ -0,0 +1,17 @@
+var message, email
+
+// message = 'Estou Estundado javascript'
+// console.log(message.substring(0,9))
+// console.log('Tamanho da mensagem: ',message.length)
+
+
+email = 'RenatoCosta@gmail.com'
+
+console.log(email.substring(0, email.indexOf('@')).toLowerCase())
+// console.log(email.substring(0, email.indexOf('@')))
+// console.log(email.substring(email.indexOf('@')))
+// console.log('BUSCAR CARACTER PELO INDICE', email.charAT(15))
+
+var nomePessoa = 'ANA PAULA'
+
+console.log('Nome Pessoa', nomePessoa.toLowerCase)
---
+++ js/se_senao.js
@@ -0,0 +1,7 @@
+var a, BUSCAR
+a = 5
+b = 7
+
+if(a < b) {
+    console.log('a é menor')
+}
