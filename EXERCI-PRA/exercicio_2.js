21825557e786ed648ceef9d19d725993406660c4
cesaryraziel <cesario.fcf@hotmail.com>
2023-10-30 17:19:23 -0300

    pratica praticando

--- EXERCI-PRA/exercicio_2.js
+++ EXERCI-PRA/exercicio_2.js
@@ -1,6 +1,6 @@
 const input = require ('readline-sync')
 
-var n1, n2, result,
+var n1, n2, result
 
 n1 = parseFloat(input.question('Informe um número: '))
 n2 = parseFloat(input.question('Informe um número: '))
